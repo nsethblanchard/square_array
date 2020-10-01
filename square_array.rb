77478b2912a3f34c4a21f6c1a60ce66f68c87b16
Seth Blanchard <nsethblanchard@gmail.com>
2020-10-01 08:03:49 -0400

    done

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+#def square_array(array)
+#  new_array = []
+#  array.each {|array| new_array << array ** 2}
+#  new_array     
+#end
+ 
 def square_array(array)
-  # your code here
+  new_array = []
+ array.each do |array|
+    new_array << array ** 2
+  end
+  new_array
 end
